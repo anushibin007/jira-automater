d682fef949837dab765e9464070de217ac135318
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2022-11-24 01:05:50 +0530

    setupEnv file to disable prop file change tracking

---
+++ setupEnv.cmd
@@ -0,0 +1,5 @@
+REM Don't track changes in any of these properties file
+git update-index --assume-unchanged src/main/resources/filtersToWatch.properties
+git update-index --assume-unchanged src/main/resources/jira-automater.properties
+git update-index --assume-unchanged src/main/resources/mail-recipients.properties
+git update-index --assume-unchanged src/main/resources/application.properties
