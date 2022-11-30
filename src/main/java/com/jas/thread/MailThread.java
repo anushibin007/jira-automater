6b37e5e5750e52c4f31c3c7385072fef4781e626
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2022-11-30 13:43:39 +0530

    Return a new thread instance every time

--- src/main/java/com/jas/thread/MailThread.java
+++ src/main/java/com/jas/thread/MailThread.java
@@ -3,12 +3,14 @@ package com.jas.thread;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.jas.pojo.MailDetails;
 import com.jas.service.MailService;
 
 @Component
+@Scope("prototype")
 public class MailThread implements Runnable {
 
 	private final Logger logger = LoggerFactory.getLogger(MailThread.class);
